b737ff4ea91e2e273c89f685f87d0163d7f004cd
El Sonny <sonny@spaceshiplabs.com>
2013-03-05 12:58:46 -0600

    import progres

---
+++ config/config.sonny_config.php
@@ -0,0 +1,20 @@
+<?php
+class sonny_config extends default_config{
+	public function sonny_config(){
+		parent::__construct();
+		//Site
+		$this->http_address = 'http://comparatuescuela/';
+		$this->mxnphp_dir = "c:/wamp/www/mxnphp/";
+		
+		//Database
+		$this->db_host = '***REMOVED***';
+		$this->db_name = 'compara';
+		$this->db_user = 'root';
+		$this->db_pass = '';
+		
+		//MXNPHP
+		$this->dev_mode = true;
+		
+	}
+}
+?>
