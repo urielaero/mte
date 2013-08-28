6fa60b0a179fa8a4c0132c9712f827530beca490
El Sonny <sonny@spaceshiplabs.com>
2013-08-28 06:55:03 -0500

    db mods
    Former-commit-id: aa5dec98b43465a5739747d2a7e07424a04fe5f1
    Former-commit-id: 67f4225a0683aaaa55af120f591bbc4c9620551b
    Former-commit-id: 5230717859d095448b0e791f73926c1a6d08ff79
    Former-commit-id: bb8bd6e30bf6ce74ca1e7c598a27f8c759192143

--- config/config.sonny_config.php
+++ config/config.sonny_config.php
@@ -7,11 +7,11 @@ class sonny_config extends default_config{
 		$this->mxnphp_dir = "c:/wamp/www/mxnphp/";
 		
 
-
+		/*
 		$this->db_host = '***REMOVED***';
 		$this->db_name = '***REMOVED***';
 		$this->db_user = '***REMOVED***';
-		$this->db_pass = '***REMOVED***';
+		$this->db_pass = '***REMOVED***';*/
 
 		//Database
 		$this->db_host = '***REMOVED***';
