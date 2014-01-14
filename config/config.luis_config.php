dbee5c9f8bbb82aa1e0291ab65fbf6b48ab9d3ff
luis <luis19prz@gmail.com>
2014-01-13 20:36:16 -0600

    cambios luis
    Former-commit-id: 34e917e796fa77c423ed26fdbe15eef37e428c60
    Former-commit-id: 446ea91361d9673b81f0cd9104a416c8f909a6ea
    Former-commit-id: da4b65027ac476597760532dc2daf13a9d0101ca
    Former-commit-id: 304448d746abee269ba3410af1a617f2af42c817

---
+++ config/config.luis_config.php
@@ -0,0 +1,30 @@
+<?php
+class luis_config extends default_config{
+	public function luis_config(){
+		parent::__construct();
+		error_reporting(0);
+		//Site
+		$this->http_address = 'http://mte.local/';
+		$this->mxnphp_dir = "c:/wamp/www/framework/";
+
+		$this->blog_address = 'http://blog.mejoratuescuela.org/';
+		
+		//Database
+		/*
+		$this->db_host = '***REMOVED***';
+		$this->db_name = 'mejoratuescuela';//enlace
+		$this->db_user = 'root';
+		$this->db_pass = '';*/
+
+
+		$this->db_host = '***REMOVED***';
+		$this->db_name = 'cte_optimizada';
+		$this->db_user = 'root';
+		$this->db_pass = '***REMOVED***';
+
+		//MXNPHP
+		$this->dev_mode = true;
+		
+	}
+}
+?>
