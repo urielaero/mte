2c8df36a34f3db748c92ba5cbf912d2b5e78a845
luis <luis19prz@gmail.com>
2014-11-18 20:49:36 -0600

    intento de fix

--- templates/mtev1/global/escuelas_excelencia.php
+++ templates/mtev1/global/escuelas_excelencia.php
@@ -3,8 +3,7 @@
 		<span class="close"></span>
 		<img src="http://3903b795d5baf43f41af-5a4e2dc33f4d93e681c3d4c060607d64.r40.cf1.rackcdn.com/disenaelcambio.png">
 	</a>
-<?php } ?>
-<?php  else if(isset($this->escuela->programas['escuelas_de_excelencia'])){ ?>
+<?php } elseif(isset($this->escuela->programas['escuelas_de_excelencia'])){ ?>
 	<a href='http://blog.mejoratuescuela.org/programa-escuelas-de-excelencia-para-abatir-el-rezago-educativo/' target='_blank' class="escuelas_excelencia">
 		<span class="close"></span>
 		<img src="http://3903b795d5baf43f41af-5a4e2dc33f4d93e681c3d4c060607d64.r40.cf1.rackcdn.com/mte2.png">
