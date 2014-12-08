ae6096db6c7b80cad95f1b9eec7d94f5f766f609
luis <luis19prz@gmail.com>
2014-12-08 16:51:06 -0600

    tabs perfil datos

--- templates/mtev2/escuelas/index.php
+++ templates/mtev2/escuelas/index.php
@@ -91,15 +91,15 @@
 						</div>
 					</div>
 					<div class="counters" layout="row" layout-sm="column">
-						<div flex><div layout="row">
+						<div flex><div layout="row" layout-margin>
 							<div flex>Número de alumnos:</div>
 							<div flex class="number">73</div>
 						</div></div>
-						<div flex><div layout="row">
+						<div flex><div layout="row" layout-margin>
 							<div flex>Número de alumnos:</div>
 							<div flex class="number">73</div>
 						</div></div>
-						<div flex><div layout="row">
+						<div flex><div layout="row" layout-margin>
 							<div flex>Número de alumnos:</div>
 							<div flex class="number">73</div>
 						</div></div>
@@ -185,6 +185,133 @@
 							</div>
 						</div>
 					</form>
+				    <md-tabs class="tabs-data" md-selected="selectedData">
+				    	<md-tab class="desempeno-tab" aria-controls="tab1-content">
+				        	Desempeño<br/>academico
+				      	</md-tab>
+				      	<md-tab class="infraestructura-tab" aria-controls="tab2-content">
+				        	Infraestructura<br/>escolar
+				      	</md-tab>
+				      	<md-tab class="comentarios-tab" aria-controls="tab2-content">
+				        	Comentarios y<br/>reportes
+				      	</md-tab>
+				    </md-tabs>
+
+				    <ng-switch on="selectedData" class="tabpanel-container">
+				        <div role="tabpanel" class="desempeno" id="profile-content" aria-labelledby="tab1" ng-switch-when="0">
+							<h2>Desempeño Académico Matutino</h2>
+							<div class="block" layout="row" layout-sm="column" layout-margin layout-fill layout-padding>
+								<div flex>
+									<div layout="row">
+										<div flex="70"><p>Número de alumnos evaluados</p></div>
+										<div flex="30" class="number"><p>73</p></div>
+									</div>
+								</div>
+								<div flex>
+									<div layout="row">
+										<div flex="70"><p>Porcentaje de alumnos en nivel reprobatorio</p></div>
+										<div flex="30" class="number"><p>0.00%</p></div>
+									</div>
+								</div>								
+							</div>
+							<div class="chart-block mate" layout="row">
+								<div class="purple" flex="25">
+									<p class="i-cont"><i class="icon-calculadora-01"></i></p>
+									<p>Resultados</p>
+									<p>ENLACE</p>
+									<div class="label"><p>Matemáticas</p></div>
+								</div>
+								<div flex="75" class="chart"></div>
+							</div>
+							<div class="chart-block espanol" layout="row">
+								<div class="purple" flex="25">
+									<p class="i-cont"><i class="icon-enlace-01"></i></p>
+									<p>Resultados</p>
+									<p>ENLACE</p>
+									<div class="label"><p>Español</p></div>
+								</div>
+								<div flex="75" class="chart"></div>
+							</div>
+						</div>
+				        <div role="tabpanel" class="infraestructura tables-box" id="profile-content" aria-labelledby="tab1" ng-switch-when="1">
+							<h2>Infraestructura escolar</h2>
+							<div class="table-top" layout="row">
+								<div flex="10" class="i-cont"><i class="icon-instalaciones-01"></i></div>
+								<div flex="90"><p>Instalaciones</p></div>
+							</div>
+							<table>
+								<tr><td>Aulas para clase</td><td>12</td></tr>
+								<tr><td>Áreas deportivas o recreativas</td><td><i class="icon-check-01"></i></td></tr>
+								<tr><td>Patio o plaza cívica</td><td><i class="icon-check-01"></i></td></tr>
+								<tr><td>Sala de computo</td><td>0</td></tr>
+								<tr><td>Cuartos para baño o sanitarios</td><td>8</td></tr>
+								<tr><td>Tazas sanitarios</td><td>17</td></tr>
+							</table>
+							<div class="table-top" layout="row">
+								<div flex="10" class="i-cont"><i class="icon-servicios-01"></i></div>
+								<div flex="90"><p>Servicios</p></div>
+							</div>
+							<table>
+								<tr><td>Energia eléctrica</td><td><i class="icon-check-01"></i></td></tr>
+								<tr><td>Servicio de agua de la red pública</td><td><i class="icon-check-01"></i></td></tr>
+								<tr><td>Drenaje</td><td><i class="icon-check-01"></i></td></tr>
+								<tr><td>Cisterna o alijibe</td><td><i class="icon-check-01"></i></td></tr>
+								<tr><td>Servicio de internet</td><td><i class="icon-tache-01"></i></td></tr>
+								<tr><td>Télefono</td><td><i class="icon-check-01"></i></td></tr>
+							</table>
+							<div class="table-top" layout="row">
+								<div flex="10" class="i-cont"><i class="icon-seguridad-01"></i></div>
+								<div flex="90"><p>Seguridad</p></div>
+							</div>
+							<table>
+								<tr><td>Señales de protección civil</td><td><i class="icon-tache-01"></i></td></tr>
+								<tr><td>Rutas de evacuación</td><td><i class="icon-check-01"></i></td></tr>
+								<tr><td>Salidas de emergencia</td><td><i class="icon-check-01"></i></td></tr>
+								<tr><td>Zonas de seguridad</td><td><i class="icon-tache-01"></i></td></tr>
+							</table>
+						</div>
+				        <div role="tabpanel" class="comentarios tables-box" id="profile-content" aria-labelledby="tab1" ng-switch-when="2">
+							<h2>Comentarios</h2>
+							<div class="table-top" layout="row">
+								<div flex="10" class="i-cont"><i class="icon-estrella-01"></i></div>
+								<div flex="90"><p>Calificación global de la escuela según usuarios</p></div>
+							</div>
+							<table>
+								<tr><td>Calificación global</td><td>10.0</td></tr>
+							</table>
+							<div class="table-top" layout="row">
+								<div flex="10" class="i-cont"><i class="icon-pregunta-01"></i></div>
+								<div flex="90"><p>Calificación promedio por pregunta</p></div>
+							</div>
+							<table>
+								<tr><td>Preparación de maestros</td><td>10.0</td></tr>
+								<tr><td>Asistencia de los maestros</td><td>10.0</td></tr>
+								<tr><td>Relación con los padres de familia</td><td>10.0</td></tr>
+							</table>
+							<div class="table-top" layout="row">
+								<div flex="10" class="i-cont"><i class="icon-comentario2-01"></i></div>
+								<div flex="90"><p>Calificación promedio por pregunta</p></div>
+							</div>
+							<ul class="comments-list">
+								<li>
+									<p><strong class="green">Calificación: 10 | </strong>Felipe (Padre de familia)</p>
+									<p>14 Ago 2014</p>
+									<p>La verdad es que esta escuela me parece de las mejores, muy recomendado</p>
+								</li>
+								<li>
+									<p><strong class="green">Calificación: 10 | </strong>Felipe (Padre de familia)</p>
+									<p>14 Ago 2014</p>
+									<p>La verdad es que esta escuela me parece de las mejores, muy recomendado</p>
+								</li>
+								<li>
+									<p><strong class="green">Calificación: 10 | </strong>Felipe (Padre de familia)</p>
+									<p>14 Ago 2014</p>
+									<p>La verdad es que esta escuela me parece de las mejores, muy recomendado</p>
+								</li>
+							</ul>
+						</div>
+					</ng-switch>
+
 				</div>
 				<div flex="25" flex-sm="100" class="sidebar">
 					<div class="box-yesno ">
