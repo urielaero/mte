e1b922d0eaef0e17c39ec2c744e0e5834c71b1ec
luis <luis19prz@gmail.com>
2014-12-15 13:20:11 -0600

    fixes css template 2;2Q

---
+++ templates/mtev2/califica_tu_escuela/califica.php
@@ -0,0 +1,37 @@
+<div class="container califica" ng-controller="escuelaCTL">
+	<div class="breadcrumb">
+		<a href="#" class="start"><i class="icon-mejora"></i></a>
+		<a href="#">Escuela</a>
+		<a href="#">Guanajuato</a>
+		<a href="#">Preparatoria Carlos Navarro</a>
+	</div>
+	<div class="menu-top">
+		<div layout="row" layout-sm="column" class="menu-row">
+			<div class="profile-title" flex="70" flex-sm="100">
+				<div class="title-container" layout="row">
+					<div flex="25" class="icon-container" hide-sm>
+						<div class="icon-wrapper vertical-align-center horizontal-align-center">
+							<i class="icon-escuela-01"></i>
+						</div>
+					</div>
+					<div flex="75" flex-sm="100">
+						<h1>Octavio Paz Locazon</h1>
+						<p>Posición estatal 3 de 95</p>
+					</div>
+				</div>			
+			</div>
+			<div class="tabs" flex="30" flex-sm="100">
+			    <md-tabs md-selected="selectedIndex">
+			    	<md-tab id="matutino-tab" aria-controls="tab1-content">
+			    		<i class="icon-matutino"></i>
+			        	Matutino
+			      	</md-tab>
+			      	<md-tab id="vespertino-tab" aria-controls="tab2-content">
+			      		<i class="icon-vespertino-01"></i>
+			        	Vespertino
+			      	</md-tab>
+			    </md-tabs>				
+			</div>
+		</div>
+	</div>
+</div>
