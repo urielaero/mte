ec3bcdb9d70ccbbf0b7ff3355eaa3aa9599be7f4
Your Name <ingbarahonacarlos@hotmail.com>
2015-01-20 16:20:21 -0600

    cambios en contanos

--- templates/mtev2/contacto/index.php
+++ templates/mtev2/contacto/index.php
@@ -1,13 +1,20 @@
 <div class='container contact'>
-	<form action="#">
-		<div layout="row" class="space-between">
-			<input type="text" class="text-input" placeholder="Nombre" flex="45">
-			<input type="mail" class="mail-input" placeholder="Correo electrónico" flex="45">
-		</div>
-		<textarea placeholder="Mensaje" cols="30" rows="7"></textarea>
-		<p>
-			<input type="submit" value="Enviar">
-			<div class="clear"></div>
-		</p>
-	</form>
+	<div class="header-form-contactanos">
+	  <div class="items-header-contact contactanos-contactanos"><h3 class="h3-contactanos">¡Contáctanos!</h3></div>
+	  <div class="items-header-contact mail-contactanos"><h3 class="h3-contactanos">mail</h3></div>
+	  <div class="items-header-contact correo-contactanos"><h3 class="h3-contactanos">contacto@mejoratuescuela.org</h3></div>
+	  <div class="items-header-contact icotelefono-contactanos"><h3 class="h3-contactanos"></h3></div>
+	  <div class="items-header-contact telefono-contactanos"><h3 class="h3-contactanos">5985-1017</h3></div>
+	</div>
+		<form action="#">
+			<div layout="row" class="space-between">
+				<input type="text" class="text-input" placeholder="Nombre" flex="45">
+				<input type="mail" class="mail-input" placeholder="Correo electrónico" flex="45">
+			</div>
+			<textarea placeholder="Mensaje" cols="30" rows="7"></textarea>
+			<p>
+				<input type="submit" value="Enviar">
+				<div class="clear"></div>
+			</p>
+		</form>
 </div>
