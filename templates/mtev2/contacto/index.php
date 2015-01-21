1b09a9ff70e6a8b676dce49c44e6b68aeb136b83
Your Name <ingbarahonacarlos@hotmail.com>
2015-01-21 11:37:16 -0600

    iconos contacto

--- templates/mtev2/contacto/index.php
+++ templates/mtev2/contacto/index.php
@@ -1,10 +1,10 @@
 <div class='container contact'>
 	<div class="header-form-contactanos">
 	  <div class="items-header-contact contactanos-contactanos"><h3 class="h3-contactanos">¡Contáctanos!</h3></div>
-	  <div class="items-header-contact mail-contactanos"><h3 class="h3-contactanos">mail</h3></div>
-	  <div class="items-header-contact correo-contactanos"><h3 class="h3-contactanos">contacto@mejoratuescuela.org</h3></div>
-	  <div class="items-header-contact icotelefono-contactanos"><h3 class="h3-contactanos"></h3></div>
-	  <div class="items-header-contact telefono-contactanos"><h3 class="h3-contactanos">5985-1017</h3></div>
+	  <div class="items-header-contact ico-contact mail-contactanos"><h1 class="h3-contactanos"><i class="icon-mail-01"></i></h1></div>
+	  <div class="items-header-contact correo-contactanos"><h4 class="h3-contactanos">contacto@mejoratuescuela.org</h4></div>
+	  <div class="items-header-contact ico-contact icotelefono-contactanos"><h1 class="h3-contactanos"><i class="icon-telefono-01"></i></h1></div>
+	  <div class="items-header-contact telefono-contactanos"><h4 class="h3-contactanos">5985-1017</h4></div>
 	</div>
 		<form action="#">
 			<div layout="row" class="space-between">
