fdeea8f304d44f51b89f72a8b259f83159a25440
el-sonny <sonny@spaceshiplabs.com>
2015-01-23 10:38:06 -0600

    bower init

--- templates/mtev2/compara/resultados-escuela.php
+++ templates/mtev2/compara/resultados-escuela.php
@@ -3,10 +3,15 @@
     window.municipios = <?= json_encode($this->municipios)?>;
     window.localidades = <?= json_encode($this->localidades)?>;
 </script>
+<script type="text/ng-template" id="mteNgSearch.html">
+	<?php $this->include_template('mteNgSearch','directives'); ?>
+</script>
 <div ng-controller='conoceCTL'>
-	<script type="text/ng-template" id="mteNgSearch.html">
-		<?php $this->include_template('mteNgSearch','directives'); ?>
-	</script>
-
+	<div class='container results mteNgSearch'>
+		<div class="breadcrumb">
+			<a href="#" class="start"><i class="icon-mejora"></i></a>
+			<a href="#">Conoce</a>
+		</div>
+	</div>
 	<div mte-ng-search objects='{municipios:municipios,entidades:entidades,localidades:localidades}'></div>
 </div>
