786eee0cf08a1f8fb6e7d2a7924c639027e736e1
Your Name <ingbarahonacarlos@hotmail.com>
2015-01-24 13:41:47 -0600

    contactanos form cambios

--- templates/mtev2/contacto/index.php
+++ templates/mtev2/contacto/index.php
@@ -1,6 +1,6 @@
 <div class='container contact'>
 	<div class="header-form-contactanos">
-	  <div class="items-header-contact contactanos-contactanos"><h3 class="h3-contactanos">¡Contáctanos!</h3></div>
+	  <div class="items-header-contact contactanos-contactanos"><h3 class="h3-contactanos-contacto">¡Contáctanos!</h3></div>
 	  <div class="items-header-contact ico-contact mail-contactanos"><h1 class="h3-contactanos"><i class="icon-mail-01"></i></h1></div>
 	  <div class="items-header-contact correo-contactanos"><h4 class="h3-contactanos">contacto@mejoratuescuela.org</h4></div>
 	  <div class="items-header-contact ico-contact icotelefono-contactanos"><h1 class="h3-contactanos"><i class="icon-telefono-01"></i></h1></div>
@@ -8,10 +8,10 @@
 	</div>
 		<form action="#" class="form-contacto">
 			<div layout="row" class="space-between">
-				<input type="text" class="text-input" placeholder="Nombre" flex="45">
-				<input type="mail" class="mail-input" placeholder="Correo electrónico" flex="45">
+				<input type="text" class="text-input forms-contactanos" placeholder="Nombre" flex="45">
+				<input type="mail" class="mail-input forms-contactanos" placeholder="Correo electrónico" flex="45">
 			</div>
-			<textarea placeholder="Mensaje" cols="30" rows="7"></textarea>
+			<textarea placeholder="Mensaje" class=" forms-contactanos" cols="30" rows="7"></textarea>
 			<p >
 				<input type="submit" value="Enviar" >
 				<div class="clear"></div>
