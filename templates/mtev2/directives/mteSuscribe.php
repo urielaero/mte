a10f2297b9ed99f6226a7c0d7aca30d2e0f312b1
Your Name <ingbarahonacarlos@hotmail.com>
2015-01-29 17:20:19 -0600

    cambios en mensaje

--- templates/mtev2/directives/mteSuscribe.php
+++ templates/mtev2/directives/mteSuscribe.php
@@ -1,8 +1,8 @@
 <div class="email-box">
 	<h4><strong ng-cloak><i class="icon-mail-01"></i>{{status}} </strong></h4>
 	<form ng-submit="submit()" ng-show="show">
-		<input type="text" ng-model="subscriber">
-		<input type="submit" value="Suscríbete" class="button-bordered">
+		<input type="text" ng-model="subscriber" placeholder="Tu Correo">
+		<input type="submit" value="Suscríbete"  class="button-bordered">
 		<md-checkbox ng-model="check" aria-label="Checkbox 1"></md-checkbox><a href="#">Aviso de privacidad</a>
 	</form>
 </div>
