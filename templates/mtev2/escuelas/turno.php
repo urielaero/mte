9e89669cd9a079601baddf99f316f8e2a51a61f8
urielaero <aero.uriel@gmail.com>
2015-02-10 10:27:36 -0600

    restablecido share options

--- templates/mtev2/escuelas/turno.php
+++ templates/mtev2/escuelas/turno.php
@@ -218,30 +218,46 @@ $urlMail = $url."#mail";
 				</div>
 			</li>
 		</ul>
-		<div class="options space-between" layout="row" layout-md="column">
-			<div flex="49" class="option">
-					<p><i class="icon-print-01"></i></p>
-					<p>Imprimir</p>
-			</div>
-			<div flex="49" class="option">
-					<p><i class="icon-share-01"></i></p>
-					<p>Compartir</p>
-			</div>
-		</div>
 				
 				<?php break;
 			}
 		?>
-				<div class="options space-between" layout="row" layout-md="column">
-			<div flex="49" class="option">
-					<p><i class="icon-print-01"></i></p>
-					<p>Imprimir</p>
+		<div class="share_options">
+			<div class="options space-between" layout="row" layout-md="column">
+				<div flex="49" class="option">
+						<p><i class="icon-print-01"></i></p>
+						<p ng-click="print();">Imprimir</p>
+				</div>
+				<div flex="49" class="option" ng-click="show_share = !show_share">
+					<span>
+						<p><i class="icon-share-01"></i></p>
+						<p>Compartir</p>
+						</span>
+				</div>
 			</div>
-			<div flex="49" class="option">
-					<p><i class="icon-share-01"></i></p>
-					<p>Compartir</p>
+			<div  class="share_show" layout="row" layout-md="column" ng-show="show_share">
+				<div flex="30" class="share_fb">
+					<a href="http://www.facebook.com/sharer/sharer.php?s=100&p[url]=<?=$urlFb?>&p[images][0]=<?=$url_logo?>&p[title]=<?=$title?>&p[summary]=<?=$description?>" target='_blank'>
+						<i class="icon-fb-01"></i>
+					</a>
+			
+				</div>
+				<div flex="30" class="share_twitter"> 
+					<a href="http://twitter.com/home?status=<?=$title." ".$urlTwitter," por @mejoratuescuela"?> " target='_blank' >
+						<i class="icon-twitter-01-01"></i>
+				
+					</a>		
+				</div>
+				<div flex="30" class="share_email">
+					<a href="mailto:?subject=<?=$title?>&amp;body=<?=$description.": ".$urlMail?>" target='_blank'>
+						<i class="icon-mail-01"></i>
+				
+					</a>		
+				</div>
 			</div>
 		</div>
+
+
 		</div>
 <!--		<ul>
 			<li class="rank1<?=$on=='Excelente'?' on':''?>">
