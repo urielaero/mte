0f583eb387c1420d7c14b62796091ec90ab431a0
Luis <luis19prz@gmail.com>
2015-02-10 16:43:01 -0500

    enlaces 404

--- templates/mtev2/home/footer-e404.php
+++ templates/mtev2/home/footer-e404.php
@@ -1,15 +1,15 @@
 <div id="footer" class="bottom-e404">
 	<div class="container e404 medium-width">
 		<div class="link">
-			<p><a href="#"><i class="icon-escuela-01"></i></a></p>
+			<p><a href="/compara"><i class="icon-escuela-01"></i></a></p>
 			<a href="/compara">Busca una escuela</a>
 		</div>
 		<div class="link">
-			<p><a href="#"><i class="icon-blog-01"></i></a></p>
+			<p><a href="/compara"><i class="icon-blog-01"></i></a></p>
 			<a href="/compara">Busca una biblioteca</a>
 		</div>
 		<div class="link">
-			<p><a href="#"><i class="icon-blog-01"></i></a></p>
+			<p><a href="/mejora"><i class="icon-blog-01"></i></a></p>
 			<a href="/mejora">Infografías</a>
 		</div>
 		<div class="clear"></div>
